ce5c31d9e62dacb2520030c4acb54dbafa9932a6
TeguhrizM19 <teguhrizqim19@gmail.com>
2024-10-26 20:57:46 +0700

    menambahkan library js

--- index.php
+++ index.php
@@ -37,6 +37,12 @@ $validated = $request->validate([
     $variable->image = $newNameImage;
     }
     -------------------------------------------------------------------------
+    <!-- Hapus -->
+    $variable = Model::find($id);
+
+    $varible->delete();
+    return redirect('/...');
+    -------------------------------------------------------------------------
     <!-- memberi alert -->
     return back()->with('pesan', 'isi pesan');
     return redirect('/...')->with('pesan', 'isi pesan');
@@ -46,3 +52,23 @@ $validated = $request->validate([
       {{ session('pesan') }}
     </div>
     @endif
+
+    <!-- Library Javascript Untuk Search Dan Pagination Tabel -->
+    <!-- ditaruh di main -->
+    @stack('styles')
+    @stack('scripts')
+
+    <!-- ditaruh disetiap tabel yg ingin diberi library -->
+    @push('scripts')
+    <script src="{{ asset('templating/plugins/datatables/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('templating/plugins/datatables-bs4/js/dataTables.bootstrap4.js') }}"></script>
+    <script>
+      $(function() {
+        $("#example2").DataTable();
+      });
+    </script>
+    @endpush
+
+    @push('styles')
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.11.3/datatables.min.css" />
+    @endpush
